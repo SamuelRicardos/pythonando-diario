591efc3072e627f035d1d67a9a16fe67f95b6cdd
SamuelRicardos <samuelric4rdo@gmail.com>
2025-01-09 19:04:19 -0300

    fix projeto - url e fechamento

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('diario.urls')),
     path('admin/', admin.site.urls),
     path('diario/', include('diario.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
